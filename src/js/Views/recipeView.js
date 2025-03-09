77b15bd1b9cdea66ab2f2c3a4b3acde22d676206
unknown <poulomi.dimpi@gmail.com>
2025-03-09 21:09:58 +0530

    committing fraction.js installed package

--- src/js/Views/recipeView.js
+++ src/js/Views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from 'fraction.js';
 
 class RecipeView extends View {
   _parentContainer = document.querySelector('.recipe');
